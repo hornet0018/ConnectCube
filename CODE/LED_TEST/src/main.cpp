c689a28f0c9a33258603673ad464b2901476c9d7
kenta-uematsu <kenta_722@yahoo.co.jp>
2023-03-04 21:21:05 +0900

    ADD 0.96LCD

--- CODE/LED_TEST/src/main.cpp
+++ CODE/LED_TEST/src/main.cpp
@@ -1,26 +1,69 @@
-#include "Arduino.h"
-#include <FastLED.h>
+#include <LovyanGFX.hpp>
+struct LGFX_Config
+{
+  static constexpr spi_host_device_t spi_host = VSPI_HOST;
+  static constexpr int dma_channel = 1;
+  static constexpr int spi_sclk = 18;
+  static constexpr int spi_mosi = 23;
+  static constexpr int spi_miso = -1;
+  static constexpr int spi_dlen = 8;
+};
 
-// LED設定
-#define NUM_LEDS 1
-#define DATA_PIN 13
-// Define the array of leds
-CRGB leds[NUM_LEDS];
+static lgfx::LGFX_SPI<LGFX_Config> lcd;
+static lgfx::Panel_ST7735S panel;
 
+//=====================setup==========================================
 void setup()
 {
-  // LED setup
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS); // GRB ordering is assumed
-  FastLED.setBrightness(50);
-}
+  panel.freq_write = 20000000;
+  panel.freq_fill = 27000000;
+  panel.freq_read = 16000000;
 
-void loop()
+  panel.spi_mode = 0;
+  panel.spi_mode_read = 0;
+
+  panel.len_dummy_read_pixel = 8;
+
+  panel.spi_read = true;
+  panel.spi_3wire = false;
+
+  panel.spi_cs = 14;
+  panel.spi_dc = 27;
+  panel.gpio_rst = 33;
+
+  panel.gpio_bl = 32;
+  panel.pwm_ch_bl = 7;
+  panel.backlight_level = true;
+
+  panel.invert = true;
+  panel.reverse_invert = true;
+  panel.rgb_order = false;
+
+  panel.memory_width = 128;
+  panel.memory_height = 160;
+  panel.panel_width = 80;
+  panel.panel_height = 160;
+  panel.offset_x = 24;
+  panel.offset_y = 0;
+
+  panel.rotation = 0;
+  panel.offset_rotation = 0;
+
+  lcd.setPanel(&panel);
+
+  SPIFFS.begin();
+
+  lcd.init();
+
+  lcd.setRotation(2);
+  lcd.setColorDepth(24);
+  lcd.fillScreen(0);
+
+  //------------------------------------------------------
+
+  // lcd.drawJpgFile(SPIFFS, "/test.jpg", 0, 0);
+}
+//============================================================
+void loop(void)
 {
-  // ゲーミング点灯
-  for (int i = 0; i <= 255; i++)
-  {
-    leds[0] = CHSV(i, 255, 255);
-    FastLED.show();
-    delay(5);
-  }
 }
